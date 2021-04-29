966eda72c64c1da9246c00b7f07905903dd844b0
VladyslavSt <vladyslav.stankevych@ukrainiansoftware.com>
2021-04-29 17:35:02 +0300

    X-Cart

---
+++ var/run/classes/XLite/Module/XPay/XPaymentsCloud/View/Checkout/Payment.php
@@ -0,0 +1,125 @@
+<?php
+// vim: set ts=4 sw=4 sts=4 et:
+
+/**
+ * Copyright (c) 2011-present Qualiteam software Ltd. All rights reserved.
+ * See https://www.x-cart.com/license-agreement.html for license details.
+ */
+
+namespace XLite\Module\XPay\XPaymentsCloud\View\Checkout;
+
+use XLite\Module\XPay\XPaymentsCloud\Main as XPaymentsCloud;
+
+/**
+ * Payment template
+ */
+abstract class Payment extends \XLite\View\Checkout\PaymentAbstract implements \XLite\Base\IDecorator
+{
+    use \XLite\Core\Cache\ExecuteCachedTrait;
+
+    /**
+     * Get CSS files
+     *
+     * @return array
+     */
+    public function getCSSFiles()
+    {
+        $list = parent::getCSSFiles();
+        if ($this->isXpaymentsMethodAvailable()) {
+            $list[] = 'modules/XPay/XPaymentsCloud/checkout/widget.css';
+        }
+        return $list;
+    }
+
+    /**
+     * Get JS files
+     *
+     * @return array
+     */
+    public function getJSFiles()
+    {
+        $list = parent::getJSFiles();
+        if ($this->isXpaymentsMethodAvailable()) {
+            $list[] = 'modules/XPay/XPaymentsCloud/checkout/widget.js';
+            $list[] = 'modules/XPay/XPaymentsCloud/checkout/apple_pay_method.js';
+        }
+        return $list;
+    }
+
+    /**
+     * Get SDK JS files
+     *
+     * @return array
+     */
+    protected function getCommonFiles()
+    {
+        $list = parent::getCommonFiles();
+        if ($this->isXpaymentsMethodAvailable()) {
+            $list[static::RESOURCE_JS][] = 'modules/XPay/XPaymentsCloud/lib/js/widget.js';
+        }
+        return $list;
+    }
+
+    /**
+     * Checks if X-Payments Cloud method is available in checkout
+     *
+     * @return bool
+     */
+    public function isXpaymentsMethodAvailable()
+    {
+        static $result = null;
+
+        if (is_null($result)) {
+            $result = false;
+            foreach ($this->getCart()->getPaymentMethods() as $method) {
+                if ('Module\XPay\XPaymentsCloud\Model\Payment\Processor\XPaymentsCloud' == $method->getClass()) {
+                    $result = true;
+                    break;
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Is "delayed payment checkout" functionality enabled (wrapper method)
+     *
+     * @return bool
+     */
+    protected function isDelayedPaymentEnabled()
+    {
+        return XPaymentsCloud::isDelayedPaymentEnabled();
+    }
+
+    /**
+     * Get Card Setup amount
+     *
+     * @return mixed
+     */
+    protected function getCardSetupAmount()
+    {
+        return $this->executeCachedRuntime(function () {
+            $result = null;
+            try {
+                $response = XPaymentsCloud::getClient()->doGetTokenizationSettings();
+                $result = $response->tokenizeCardAmount;
+            } catch (\XPaymentsCloud\ApiException $e) {
+                XPaymentsCloud::log($e->getMessage());
+            }
+
+            return $result;
+        });
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getXpaymentsCartTotal()
+    {
+        return $this->isDelayedPaymentEnabled()
+            ? $this->getCardSetupAmount()
+            : $this->getCart()->getTotal();
+    }
+
+}
---
+++ var/run/skins/97/97d5e4da6879ef43d5e66cc4e9c641847975bce31a689222b28c9d494bfb13d9.php
@@ -0,0 +1,66 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* /home/ruslan/Projects/next/output/xcart/src/skins/admin/modules/XC/Stripe/welcome_block/vendor_stripe/block.twig */
+class __TwigTemplate_92a7054d383d4d92991114435726d46274cb03a4b45cacefa7ad62aa45eda210 extends \XLite\Core\Templating\Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 6
+        echo "
+<div class=\"block-content\">
+    ";
+        // line 8
+        echo XLite\Core\Templating\Twig\Extension\xcart_twig_escape_filter($this->env, call_user_func_array($this->env->getFunction('widget_list')->getCallable(), [$this->env, $context, [0 => "welcome-block.vendor-stripe.content"]]), "html", null, true);
+        echo "
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "/home/ruslan/Projects/next/output/xcart/src/skins/admin/modules/XC/Stripe/welcome_block/vendor_stripe/block.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  34 => 8,  30 => 6,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "/home/ruslan/Projects/next/output/xcart/src/skins/admin/modules/XC/Stripe/welcome_block/vendor_stripe/block.twig", "");
+    }
+}
